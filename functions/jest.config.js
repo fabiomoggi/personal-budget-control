ca805440c69fa90f50eb3fa39c73170bd96ece18
Fabio Moggi <fabio.moggi@gmail.com>
2025-02-22 22:13:30 -0300

    jest 2

--- functions/jest.config.js
+++ functions/jest.config.js
@@ -2,5 +2,8 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testMatch: ['**/__tests__/**/*.test.ts', '**/?(*.)+(spec|test).ts'],
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageReporters: ['lcov']
   //setupFilesAfterEnv: ['<rootDir>/setupTests.js']
 };
