ec62b81aa9179737f3ecc6432254114fbe7340b2
Fabio Moggi <fabio.moggi@gmail.com>
2025-03-29 14:39:51 -0300

    fix uncovered test code

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,11 +1,3 @@
-import { onRequest } from "firebase-functions/v2/https";
-import { logger } from "firebase-functions";
-
-export const helloWorld = onRequest((req, res) => {
-  logger.info("Hello logs!", { structuredData: true });
-  res.send("Hello, my fucking world with SONAR QUBE!");
-});
-
 export const getName = (): string => {
   return "Firebase" + "";
 };
