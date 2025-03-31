94038331f00b84cb7b88481e89743317c27c5fa1
Fabio Moggi <fabio.moggi@gmail.com>
2025-03-30 22:09:58 -0300

    test ok

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -13,3 +13,7 @@ export const getSum = (a: number, b: number): number => {
 export const getFullName = (): string => {
   return "Firebase";
 };
+
+export const getFirstName = (): string => {
+  return "frstname";
+};
